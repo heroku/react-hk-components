3771db599fdbc6dbca450b74be688a61f3002062
Casey Watts <casey.s.watts@gmail.com>
2018-02-01 11:16:12 -0500

    introduce ModalFooter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as HKButton } from './HKButton'
 export { default as Modal } from './Modal'
 export { default as ModalHeader } from './ModalHeader'
+export { default as ModalFooter } from './ModalFooter'
