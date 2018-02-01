bb63463a12c44644bddc31cb26facd884c68c397
Casey Watts <casey.s.watts@gmail.com>
2018-02-01 11:17:35 -0500

    rename all the modal components to HKModal

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as HKButton } from './HKButton'
-export { default as Modal } from './Modal'
-export { default as ModalHeader } from './ModalHeader'
-export { default as ModalFooter } from './ModalFooter'
+export { default as HKModal } from './HKModal'
+export { default as HKModalHeader } from './HKModalHeader'
+export { default as HKModalFooter } from './HKModalFooter'
