37311230426d85b36870182cecbb2d9f9e9883a3
Maciek Sakrejda <maciek@heroku.com>
2018-02-02 15:10:12 -0800

    Also inline the HKModal header
    This is also a pretty trivial component that is not intended for
    outside consumption in isolation.

--- src/HKModal.tsx
+++ src/HKModal.tsx
@@ -2,8 +2,6 @@ import { MalibuIcon } from '@heroku/react-malibu'
 import * as React from 'react'
 import SRMModal from 'simple-react-modal'
 
-import HKModalHeader from './HKModalHeader'
-
 interface IModalProps {
   children: React.ReactNode,
   header: React.ReactNode,
@@ -13,8 +11,33 @@ interface IModalProps {
 }
 
 export default class Modal extends React.Component<IModalProps, {}> {
+
+  header () {
+    const { header, onDismiss } = this.props
+    return (<div className="bg-near-white dark-gray bb b--light-silver f4 flex items-center justify-center relative br--top br2">
+      {header
+        ? (<div className="hk-modal-header pa4 bg-near-white">
+          {header}
+        </div>)
+        : null}
+      {onDismiss
+        ? (<div className="right-1 h-100 absolute pointer" onClick={onDismiss}>
+          <MalibuIcon name='delete-16' fillClass='dark-gray' extraClasses="icon malibu-icon h1 w1 fill-dark-gray o-50 hover-o-100 h-100 v-mid"/>
+        </div>)
+        : null}
+    </div>)
+  }
+
+  footer () {
+    return (<div className="bt b--light-silver w-100 pa3 tr">
+      {this.props.footer}
+    </div>)
+
+  }
+
   public render () {
-    const { show, children, onDismiss, header, footer } = this.props
+    const { show, children, onDismiss } = this.props
+
     return (
       <SRMModal
         containerStyle={{}}
@@ -23,16 +46,13 @@ export default class Modal extends React.Component<IModalProps, {}> {
         className="flex flex-column items-center justify-center"
         closeOnOuterClick={true}
         show={show}
-        onClose={onDismiss}
-      >
-        <HKModalHeader onDismiss={onDismiss}>
-          {header}
-        </HKModalHeader>
+        onClose={onDismiss}>
+
+        {this.header()}
 
         {children}
-        <div className="bt b--light-silver w-100 pa3 tr">
-          {footer}
-        </div>
+
+        {this.footer()}
       </SRMModal>
     )
   }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as HKButton } from './HKButton'
 export { default as HKModal } from './HKModal'
-export { default as HKModalHeader } from './HKModalHeader'
