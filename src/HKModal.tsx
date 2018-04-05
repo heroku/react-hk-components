b6d48574dc729ae95ff2fb726b38b5d7dbc2dc40
Maciek Sakrejda <m.sakrejda@gmail.com>
2018-04-05 10:35:17 -0700

    Streamline HKModal (#16)
    The extra classes don't buy us much and are not worth the complexity
    of spreading this out across multiple files.

--- src/HKModal.tsx
+++ src/HKModal.tsx
@@ -1,8 +1,6 @@
 import { MalibuIcon } from '@heroku/react-malibu'
 import * as React from 'react'
 import SRMModal from 'simple-react-modal'
-import HKModalHeader from './HKModalHeader'
-import HKModalFooter from './HKModalFooter'
 
 interface IModalProps {
   children: React.ReactNode,
@@ -12,28 +10,39 @@ interface IModalProps {
   show: boolean,
 }
 
-export default class Modal extends React.Component<IModalProps, {}> {
+export default class HKModal extends React.Component<IModalProps, {}> {
   public render () {
     const { show, children, onDismiss, header, footer } = this.props
+    const headerElem = header && (
+      <div className='hk-modal-header pa4 bg-near-white'>
+        {header}
+      </div>
+    )
+    const dismissElem = onDismiss && (
+      <div className='right-1 h-100 absolute pointer' onClick={onDismiss}>
+        <MalibuIcon name='delete-16' fillClass='dark-gray' extraClasses='icon malibu-icon h1 w1 fill-dark-gray o-50 hover-o-100 h-100 v-mid'/>
+      </div>
+    )
     return (
       <SRMModal
         containerStyle={{}}
-        containerClassName="w-100 mw7 center bg-white shadow-outer-1 br1 relative"
-        style={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, "zIndex": 9999, background: 'rgba(0,0,0,.2)' }}
-        className="flex flex-column items-center justify-center"
+        containerClassName='w-100 mw7 center bg-white shadow-outer-1 br1 relative'
+        style={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, zIndex: 9999, background: 'rgba(0,0,0,.2)' }}
+        className='flex flex-column items-center justify-center'
         closeOnOuterClick={true}
         show={show}
         onClose={onDismiss}
       >
-        <HKModalHeader onDismiss={onDismiss}>
-          {header}
-        </HKModalHeader>
+        <div className='bg-near-white dark-gray bb b--light-silver f4 flex items-center justify-center br--top br2'>
+          {headerElem}
+          {dismissElem}
+        </div>
 
         {children}
 
-        <HKModalFooter>
+        <div className='bt b--light-silver w-100 pa3 tr'>
           {footer}
-        </HKModalFooter>
+        </div>
       </SRMModal>
     )
   }
