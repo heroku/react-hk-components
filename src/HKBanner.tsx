a84dd6e6b04905e89130e31bd0b2eed309321b52
Idan Gazit <idan@gazit.me>
2018-07-31 14:33:27 +0300

    Add HKBanner

---
+++ src/HKBanner.tsx
@@ -0,0 +1,67 @@
+import { MalibuIcon } from '@heroku/react-malibu'
+import * as classnames from 'classnames'
+import * as React from 'react'
+import {
+  default as HKButton,
+  Type as ButtonType,
+} from './HKButton'
+
+export enum BannerType {
+  generic = 'generic',
+  info = 'info',
+  success = 'success',
+  warning = 'warning',
+  danger = 'danger',
+}
+
+interface IBannerProps {
+  buttonText?: string,
+  title?: string,
+  children: JSX.Element | JSX.Element[] | string,
+  icon: string,
+  onClick?: () => void,
+  type: BannerType,
+}
+
+export default class HKBanner extends React.Component<IBannerProps> {
+  private typeMap = new Map<BannerType, ButtonType>([
+    [BannerType.generic, ButtonType.Secondary],
+    [BannerType.info, ButtonType.Info],
+    [BannerType.success, ButtonType.Success],
+    [BannerType.warning, ButtonType.Warning],
+    [BannerType.danger, ButtonType.Danger],
+  ])
+
+  private colorMap = {
+    danger: 'red',
+    generic: 'dark-gray',
+    info: 'blue',
+    success: 'green',
+    warning: 'orange',
+  }
+
+  public render () {
+    const { buttonText, children, type, icon, onClick, title } = this.props
+    let button
+    if (buttonText) {
+      button = <HKButton type={this.typeMap.get(type)} onClick={this.handleClick} className='flex-none'>{buttonText}</HKButton>
+    }
+
+    return (
+      <div className={`hk-banner--${type} flex items-center`}>
+        <MalibuIcon name={icon} size={24} fillClass={this.colorMap[type]} extraClasses='mr2 flex-none' />
+        <div className='flex-auto'>
+          {title && <div className='b'>{title}</div>}
+          {this.props.children}
+        </div>
+        {button}
+      </div>
+    )
+  }
+
+  private handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (this.props.onClick) {
+      this.props.onClick()
+    }
+  }
+}
---
+++ stories/HKBanner.stories.tsx
@@ -0,0 +1,40 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+
+import {
+  BannerType,
+  default as HKBanner,
+} from '../src/HKBanner'
+
+const types = [
+  BannerType.danger,
+  BannerType.generic,
+  BannerType.info,
+  BannerType.success,
+  BannerType.warning,
+]
+
+const typeMap = {
+  danger: 'warning-ring-28',
+  generic: 'logo-block-28',
+  info: 'info-ring-28',
+  success: 'success-ring-28',
+  warning: 'warning-ring-28',
+
+}
+
+types.forEach((type) => {
+  const stories = storiesOf(`HKBanner/${type}`, module)
+  stories.add(`default`, () => (
+    <HKBanner icon={typeMap[type]} type={type} >Lorem ipsum dolor sit amet.</HKBanner>
+  ))
+  stories.add(`with title`, () => (
+    <HKBanner icon={typeMap[type]} type={type} title='This is the title line'>Lorem ipsum dolor sit amet.</HKBanner>
+  ))
+  stories.add(`with button`, () => (
+    <HKBanner icon={typeMap[type]} type={type} buttonText='Click Me'>Lorem ipsum dolor sit amet.</HKBanner>
+  ))
+  stories.add(`with title and button`, () => (
+    <HKBanner icon={typeMap[type]} type={type} title='This is the title line' buttonText='Click Me'>Lorem ipsum dolor sit amet.</HKBanner>
+  ))
+})
