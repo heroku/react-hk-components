722fb2bb6283cada09819ada7f42a318ad5ec553
Idan Gazit <idan@gazit.me>
2018-08-07 02:04:22 +0300

    Make HKModal a controlled component again
    Also update stories to properly consume the button events.

--- stories/HKModal.stories.tsx
+++ stories/HKModal.stories.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 
+import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 
-import { default as HKButton } from '../src/HKButton'
+import {
+  default as HKButton,
+  Type as ButtonType,
+} from '../src/HKButton'
 import { default as HKModal, Type } from '../src/HKModal'
 
 interface IModalWrapperProps {
@@ -38,7 +42,20 @@ class ModalWrapper extends React.Component<
           show={this.state.showModal}
           onDismiss={this.handleModalDismiss}
           header={<div>header text</div>}
-          footer={<HKButton>Submit</HKButton>}
+          buttons={[
+            {
+              disabled: false,
+              text: 'Cancel',
+              type: ButtonType.Tertiary,
+              value: 'cancel',
+            },
+            {
+              disabled: false,
+              text: 'OK',
+              type: this.props.type === Type.Destructive ? ButtonType.Danger : ButtonType.Primary,
+              value: 'ok',
+            },
+          ]}
         >
           <div className='pa6'>with some important details here below</div>
         </HKModal>
@@ -46,8 +63,9 @@ class ModalWrapper extends React.Component<
     )
   }
 
-  private handleModalDismiss = () => {
+  private handleModalDismiss = (value?: string) => {
     this.setState({ showModal: false })
+    action('Modal Dismiss')(value)
   }
 
   private showModal = () => {
