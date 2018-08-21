1e1efcf389a434174e7dd84779a1ca38f8e3bc40
Victoria Li <li.victoria51@gmail.com>
2018-08-21 10:34:42 -0700

    Adds data-testID attributes for testing components (#42)

--- src/HKDropdown.tsx
+++ src/HKDropdown.tsx
@@ -19,6 +19,7 @@ interface IDropdownProps {
   closeOnClick?: boolean, // hide dropdown content after onClick in dropdown content
   contentClassName?: string, // dropdown content styling
   disabled?: boolean,
+  name?: string, // name of the dropdown, used for testing in testID
   title?: string,
 }
 
@@ -30,6 +31,7 @@ export default class HKDropdown extends React.Component<IDropdownProps, IDropdow
   public static defaultProps = {
     closeOnClick: true,
     disabled: false,
+    name: 'hkdropdown',
   }
 
   public state = {
@@ -41,16 +43,16 @@ export default class HKDropdown extends React.Component<IDropdownProps, IDropdow
   public handleContentClick = () => this.props.closeOnClick && this.setState({ showDropdown: false })
 
   public render () {
-    const { align, children, className, contentClassName, disabled, title } = this.props
+    const { align, children, className, contentClassName, disabled, name, title } = this.props
     const { showDropdown } = this.state
     const alignDropdown = align ? `hk-dropdown--${align}` : `hk-dropdown`
     return (
       <div className='relative dib'>
-        <HKButton className={classnames({ ph1: !title }, className)} type={Type.Secondary} disabled={disabled} onClick={this.handleDropdown}>
+        <HKButton data-testid={`${name}-dropdown-button`} className={classnames({ ph1: !title }, className)} type={Type.Secondary} disabled={disabled} onClick={this.handleDropdown}>
           {title}
           <MalibuIcon key='icon' name='caret-16' size={16} fillClass='fill-purple' extraClasses={classnames({ pl1: title })} />
         </HKButton>
-        <div onClick={this.handleContentClick}>
+        <div data-testid={`${name}-dropdown-content`} onClick={this.handleContentClick}>
           {
             showDropdown &&
             (<ul className={classnames(alignDropdown, contentClassName)}>
