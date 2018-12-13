c78c1fd94148ec6d28b8ee1b93debde30749ea5a
Maciek Sakrejda <m.sakrejda@gmail.com>
2018-12-13 15:05:43 -0800

    Fix charting lodash shenanigans (#64)
    Drop the "break lodash" plugin. It breaks lodash.
    
    We were not quite able to figure out what's going on, but apparently
    this plugin, while it does improve our bundle size slightly, also
    makes a total mess of (some?) lodash methods, which led to very
    strange behavior when RHKC was used in DHC (and probably elsewhere).
    
    After removing the plugin, we noticed we could rely on browsers' built-in
    Array.prototype.flatMap, but it turns out node does not define that so it
    broke our tests, so we'll keep using lodash.
    
    Also some minor refactoring in related code, and move lodash from
    devDependencies where we mistakenly had it to a proper dependency.

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,41 @@ const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: {
-    'react-hk-components': './src/index.ts'
+    'react-hk-components': './src/index.ts',
   },
   output: {
     path: path.resolve(__dirname, './dist/umd'),
     filename: '[name].js',
     libraryTarget: 'umd',
-    library: 'react-hk-components'
+    library: 'react-hk-components',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
         uglifyOptions: {
-          keep_fnames: true
-        }
-      })
-    ]
+          keep_fnames: true,
+        },
+      }),
+    ],
   },
   plugins: [
-    new LodashModuleReplacementPlugin(),
     new CopyWebpackPlugin([
       {
         from: path.resolve(__dirname, './src/static'),
-        to: path.resolve(__dirname, './dist/static')
-      }
+        to: path.resolve(__dirname, './dist/static'),
+      },
     ]),
     new MiniCssExtractPlugin({
       filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
+      chunkFilename: '[id].css',
+    }),
   ],
   module: {
     rules: [
@@ -48,18 +46,18 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           configFileName: 'tsconfig.build.json',
-          silent: true
-        }
+          silent: true,
+        },
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
-          'css-loader'
-        ]
-      }
-    ]
-  }
+          'css-loader',
+        ],
+      },
+    ],
+  },
 }
