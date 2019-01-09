d9fd4bf27159f673d7a6362a954734cd72fbc0c4
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2019-01-09 15:08:22 -0500

    Scroll table into view onPageChange (#67)

--- src/HKTable.tsx
+++ src/HKTable.tsx
@@ -5,7 +5,26 @@ import './static/styles/table.css'
 import { default as HKTablePagination } from './HKTablePagination'
 
 const HKTable: React.FunctionComponent<any> = props => {
-  return <ReactTable {...props} PaginationComponent={HKTablePagination} />
+  const tableRef = React.createRef<HTMLDivElement>()
+
+  const handlePageChange = (...args) => {
+    if (tableRef.current) {
+      tableRef.current.scrollIntoView()
+    }
+    if (props.onPageChange) {
+      props.onPageChange(...args)
+    }
+  }
+
+  return (
+    <div ref={tableRef}>
+      <ReactTable
+        {...props}
+        onPageChange={handlePageChange}
+        PaginationComponent={HKTablePagination}
+      />
+    </div>
+  )
 }
 
 HKTable.displayName = 'HKTable'
