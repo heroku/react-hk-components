dea3438ac9ec6dd42c6d46174d42ef1d6af30db9
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2019-02-13 09:40:55 -0500

    Expose className prop on HKTableHeader (#75)

--- stories/HKTableHeader.stories.tsx
+++ stories/HKTableHeader.stories.tsx
@@ -17,3 +17,11 @@ storiesOf('HKTableHeader', module)
   .add(`Unsorted`, () => <HKTableHeader label='Title' id='title' />)
   .add(`Desc`, () => <HKTableHeader label='Title' id='title' sort={desc} />)
   .add(`Asc`, () => <HKTableHeader label='Title' id='title' sort={asc} />)
+  .add(`With custom classes`, () => (
+    <HKTableHeader
+      className='justify-end'
+      label='Aligned Right!'
+      id='title'
+      sort={asc}
+    />
+  ))
