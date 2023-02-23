d99119935c3e8fc9c86078d2d05413a26a970e30
Kenya <51925141+kenyaplenty@users.noreply.github.com>
2023-02-23 12:18:37 -0500

    Removed irrelevant enzyme dependency

--- test/test-setup.ts
+++ test/test-setup.ts
@@ -1,6 +1,2 @@
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 registerRequireContextHook()
-
-Enzyme.configure({ adapter: new Adapter() })
